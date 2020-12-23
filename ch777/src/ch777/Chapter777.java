03c9b303f7ef3d154b9a149dbb459733228f456e
justNOZA <1995pjs2635@gmail.com>
2020-12-23 16:31:14 +0900

    Java Study and Git Test

---
+++ ch777/src/ch777/Chapter777.java
@@ -0,0 +1,11 @@
+package ch777;
+
+public class Chapter777 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's start Java");
+		System.out.println("Test Egit");
+	}
+
+}
