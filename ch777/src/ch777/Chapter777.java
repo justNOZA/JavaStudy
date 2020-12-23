c0f1165eb1f65230f0f439695975ba01dba52103
justNOZA <1995pjs2635@gmail.com>
2020-12-23 16:40:41 +0900

    수정

--- ch777/src/ch777/Chapter777.java
+++ ch777/src/ch777/Chapter777.java
@@ -5,7 +5,6 @@ public class Chapter777 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Let's start Java");
-		System.out.println("Test Egit");
 	}
 
 }
