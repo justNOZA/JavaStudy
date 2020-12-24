6bbf6773ea5c94e767628d8c7008d99e1609e06d
justNOZA <1995pjs2635@gmail.com>
2020-12-24 20:27:36 +0900

    배열 1문제 가장 깔끔한 답변

---
+++ Programmers/src/Sort01A.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Sort01A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,5,2,6,3,7,4};
+		int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+		
+		int[] answer = solution( array, commands);
+	}
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
