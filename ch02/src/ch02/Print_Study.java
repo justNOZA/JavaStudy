95c937f0a2672740fe8f2ebab01e02cf768b6b0f
justNOZA <1995pjs2635@gmail.com>
2020-12-26 16:55:39 +0900

    java chpater2

---
+++ ch02/src/ch02/Print_Study.java
@@ -0,0 +1,21 @@
+package ch02;
+
+public class Print_Study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("console print_Enter Line");
+		System.out.println("console print");
+		System.out.print("console print_ Not Eenter");
+		System.out.print("console_print");
+		
+		System.out.println("3+5");	//¹®ÀÚ¿­ Ãâ·Â
+		System.out.println(3+5);	//µ¡¼À
+		System.out.println(3/5);	//³ª´°¼À
+		System.out.println(3%5);	//³ª¸ÓÁö
+		System.out.println(3-5);	//»¬¼À
+		System.out.println(3*5);	//°ö¼À
+				
+	}
+
+}
---
+++ ch02/src/ch02/Variable_Study.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Variable_Study {
+
+	public static void main(String[] args) {
+		int x = 0, y = 3; //변수 선언 및 초기화
+
+		System.out.println(x); //x출력
+		System.out.println(y); //y출력
+		System.out.println(x+y); //x와 y의 계산
+		
+	}
+}
