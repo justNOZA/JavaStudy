1b382f2b44e8abcb6e9c23b51865f4cc4fc0dc4a
justNOZA <1995pjs2635@gmail.com>
2021-04-25 22:46:23 +0900

    queestion 14

--- firstStep/src/firstStep/basicQuestion14.java
+++ firstStep/src/firstStep/basicQuestion14.java
@@ -4,7 +4,19 @@ public class basicQuestion14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] list = new int[50];
+		for(int i = 0; i < list.length; i++) {
+			list[i] = (int)(Math.random() * 101);
+		}
+		for(int i = 0; i < list.length; i += 6) {
+			for(int j = i; j < i + 6; j++) {
+				if(j == list.length) {
+					break;
+				}
+				System.out.print(list[j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
