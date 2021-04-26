4dbf3d0bb05b715d18f6f6a3aab0be7eab27d3c0
justNOZA <1995pjs2635@gmail.com>
2021-04-27 08:16:06 +0900

    문제 16번

--- firstStep/src/firstStep/Question16.java
+++ firstStep/src/firstStep/Question16.java
@@ -4,7 +4,11 @@ public class Question16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i = 1; i < 101; i++) {
+			if(i%3 == 0 && i%7 ==0) {
+				System.out.print(i+" ");
+			}
+		}
 	}
 
 }
