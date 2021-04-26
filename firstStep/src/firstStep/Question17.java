96e757b477a4a4d07be14016962b5d1e5da8fe22
justNOZA <1995pjs2635@gmail.com>
2021-04-27 08:20:35 +0900

    문제 17번

--- firstStep/src/firstStep/Question17.java
+++ firstStep/src/firstStep/Question17.java
@@ -1,10 +1,21 @@
 package firstStep;
 
+import java.util.Scanner;
+
 public class Question17 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("X값을 입력하시오 : ");
+		 int x = sc.nextInt();
+		 System.out.print("\nY값을 입력하시오 : ");
+		 int y = sc.nextInt();
+		 int result = 1;
+		 for(int i = 0; i < y; i++) {
+			 result *= x;
+		 }
+		 System.out.println(x+"의 "+y+"승은 "+result);
 	}
 
 }
