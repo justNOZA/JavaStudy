b9236d824bce5c7c0ddb785536b358f69c29da37
justNOZA <1995pjs2635@gmail.com>
2021-05-03 17:57:41 +0900

    문제 20

--- firstStep/src/firstStep/Question20.java
+++ firstStep/src/firstStep/Question20.java
@@ -4,7 +4,19 @@ public class Question20 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] result = output(10,"#");
+		for(int i = 0; i < result.length; i++) {
+			System.out.print(result[i]+" ");
+		}
+	}
 
+	private static String[] output(int i, String string) {
+		// TODO Auto-generated method stub
+		String[] result = new  String[i];
+		for(int j = 0; j < result.length; j++) {
+			result[j] = string;
+		}
+		return result;
 	}
 
 }
