a36e805b77430c551ddecb0b7f7c169c590cab06
justNOZA <1995pjs2635@gmail.com>
2021-05-03 17:54:24 +0900

    문제 19번

--- firstStep/src/firstStep/Question19.java
+++ firstStep/src/firstStep/Question19.java
@@ -4,7 +4,31 @@ public class Question19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("더하기 : "+sum(3,5));
+		System.out.println("빼기 : "+sub(7,2));
+		System.out.println("곱하기 : "+mul(4,5));
+		System.out.println("나누기 : "+div(7,3));
+		
+	}
 
+	private static String div(int i, int j) {
+		// TODO Auto-generated method string
+		return String.format("%.2f", ((double)i/j));
+	}
+
+	private static String mul(int i, int j) {
+		// TODO Auto-generated method stub
+		return ""+(i*j);
+	}
+
+	private static String sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return ""+(i-j);
+	}
+
+	private static String sum(int i, int j) {
+		// TODO Auto-generated method stub
+		return ""+(i+j);
 	}
 
 }
