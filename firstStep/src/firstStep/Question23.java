c8e3ebc5f344fe2ccd39bf7a9880e3dcc97ad9ff
justNOZA <1995pjs2635@gmail.com>
2021-05-07 22:46:14 +0900

    푸는중

--- firstStep/src/firstStep/Question23.java
+++ firstStep/src/firstStep/Question23.java
@@ -1,10 +1,24 @@
 package firstStep;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+
 public class Question23 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LinkedHashMap<String, Integer> list = new LinkedHashMap<>();
+		list = inputData(3);
+	}
 
+	private static LinkedHashMap<String, Integer> inputData(int i) {
+		// TODO Auto-generated method stub
+		LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+		for(int j = 0; j < i; j++) {
+			result.put(key, value)
+		}
+		return null;
 	}
 
 }
