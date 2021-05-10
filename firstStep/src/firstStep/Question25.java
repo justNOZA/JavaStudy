ed755d2479ddbd1756388c2050d13d47b72b2154
justNOZA <1995pjs2635@gmail.com>
2021-05-10 20:43:39 +0900

    문제 25

--- firstStep/src/firstStep/Question25.java
+++ firstStep/src/firstStep/Question25.java
@@ -1,10 +1,16 @@
 package firstStep;
 
+import java.util.Scanner;
+
 public class Question25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int	[]month={31,28,31,30,31,30,31,31,30,31,30,31};
+		Scanner sc = new Scanner(System.in);
+		System.out.print("원하는 달 : ");
+		int check = sc.nextInt();
+		System.out.printf("\n원하는 달 %d월은 %d일입니다.",check,month[check-1]);
 	}
 
 }
