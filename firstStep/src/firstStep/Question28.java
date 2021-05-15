da956d9fb8bf1bd6646326c79035f4476297f667
justNOZA <1995pjs2635@gmail.com>
2021-05-15 17:26:14 +0900

    문제 28

--- firstStep/src/firstStep/Question28.java
+++ firstStep/src/firstStep/Question28.java
@@ -4,7 +4,15 @@ public class Question28 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Test28 ob = new Test28();
+		print(ob);
+	}
 
+	private static void print(Test28 ob) {
+		// TODO Auto-generated method stub
+		System.out.printf("»ï°¢ÇüÀÇ ³ĞÀÌ : %.2f",ob.getArea());
+		System.out.printf("\nºøº¯±æÀÌ : %.2f",ob.getHypotenuse());
+		System.out.printf("\nµÑ·¹±æÀÌ:  %.2f",ob.getPerimeter());
 	}
 
 }
@@ -13,12 +21,12 @@ class Test28{
 	double base = 5.0;
 	double height = 3.5;
 	public double getArea() {
-		return 0;
+		return base * height / 2;
 	}
 	public double getHypotenuse() {
-		return 0;
+		return Math.sqrt(base*base+height*height);
 	}
 	public double getPerimeter() {
-		return 0;
+		return base+height+this.getHypotenuse();
 	}
 }
